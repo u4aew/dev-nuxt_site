55bfdf6e1ea1a28bf8648ea1130eaaf96e074d30
uchaev.s <dialoggg1994@gmail.com>
2019-10-30 23:36:29 +0700

    test config

--- plugins/helpers/server/city-helper.js
+++ plugins/helpers/server/city-helper.js
@@ -13,16 +13,15 @@ const MASK = 'http://';
 class CityHelper {
   static getCity($req, $env) {
     if (process.server) {
-      if ($req.headers.host === $env.baseUrl.replace(MASK, '')) {
-        return CITIES[0]
-      }
-
       const city = $req.headers.host.split('.')[0];
       if (city.length > 0) {
-        return CITIES.find(item => item.code === city)
-      } else {
-        return null
+        if (typeof CITIES.find(item => item.code === city) !== 'undefined') {
+          return CITIES.find(item => item.code === city)
+        } else {
+          return CITIES[0]
+        }
       }
+      return CITIES[0]
     }
   }
 }
