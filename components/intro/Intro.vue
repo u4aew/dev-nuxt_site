cc5bcf874dffd96631aa482d62225d55450a9041
uchaev.s <dialoggg1994@gmail.com>
2019-11-04 23:47:23 +0700

    add log

--- components/intro/Intro.vue
+++ components/intro/Intro.vue
@@ -3,7 +3,7 @@
     <div class="intro__wrapper">
       <div class="intro__title">
         <h1 class="title-h1">
-          Ломприём dfsfd asdadasasd
+          Ломприём dfsfd asdadasasd dfsffddsf 999
         </h1>
       </div>
       <div class="intro__desc">
