c3ac46af0a09823f0ab6bdc6055c0ff74835aa8d
uchaev.s <dialoggg1994@gmail.com>
2019-11-08 21:37:26 +0700

    test ip

--- store/index.js
+++ store/index.js
@@ -15,10 +15,7 @@ export const mutations = {
 export const actions = {
   async nuxtServerInit({commit}, {req, env}) {
 
-    const ip = req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    const ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
 
     commit('SET_CITY', CityHelper.getCity(req, env));
     commit('SET_IP', ip)
