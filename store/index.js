a2a472efb4e037cdc9ae1be9aaa167e7f252a261
uchaev.s <dialoggg1994@gmail.com>
2019-11-08 22:41:17 +0700

    инфа по городу

--- store/index.js
+++ store/index.js
@@ -18,7 +18,7 @@ export const mutations = {
 export const actions = {
   async nuxtServerInit({commit}, {req, env, $axios}) {
     const ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
-    const cityInfo = await $axios.$get(`${process.env.GEO_API_URL}/${ip}`);
+    const cityInfo = 'asds'
 
     commit('SET_CITY', CityHelper.getCity(req, env));
 
