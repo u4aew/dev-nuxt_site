5c19ce3dd196bfd230f3f97faf95da88d6494266
uchaev.s <dialoggg1994@gmail.com>
2019-11-14 22:22:54 +0700

    Добавил статьи

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     title: 'test',
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1280'},
+      {name: 'viewport', content: 'width=1280, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'Nuxt.js project'}
     ],
     link: [
